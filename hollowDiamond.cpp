90354d16e8fafca603e70a473819ad900c3c9868
MangoCode0 <malik.asgar@outlook.com>
2025-06-23 14:02:03 +0530

    hollowdiomond

---
+++ hollowDiamond.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n=6;
+    // cout << "Enter the number of rows for half of the diamond: ";
+    // cin >> n;
+
+    // Upper half
+    for (int i = 1; i <= n; i++) {
+        // Print spaces
+        for (int j = 1; j <= n - i; j++) {
+            cout << " ";
+        }
+        // Print stars and spaces
+        for (int j = 1; j <= 2 * i - 1; j++) {
+            if (j == 1 || j == 2 * i - 1) {
+                cout << "*";
+            } else {
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
+
+    // Lower half
+    for (int i = n - 1; i >= 1; i--) {
+        // Print spaces
+        for (int j = 1; j <= n - i; j++) {
+            cout << " ";
+        }
+        // Print stars and spaces
+        for (int j = 1; j <= 2 * i - 1; j++) {
+            if (j == 1 || j == 2 * i - 1) {
+                cout << "*";
+            } else {
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
